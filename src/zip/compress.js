a866702ad8c90387b9406281bca032bbb98a962c
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2024-01-30 02:28:41 +0600

    feat: Finishied Zip module

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,21 @@
+import fs from "fs";
+import path from "path";
+import zlib from "zlib";
+
 const compress = async () => {
-    // Write your code here 
+  const inputFilePath = path.resolve(
+    "src",
+    "zip",
+    "files",
+    "fileToCompress.txt"
+  );
+  const outputFilePath = path.resolve("src", "zip", "files", "archive.gz");
+
+  const readStream = fs.createReadStream(inputFilePath);
+  const gzipStream = zlib.createGzip();
+  const writeStream = fs.createWriteStream(outputFilePath);
+
+  readStream.pipe(gzipStream).pipe(writeStream);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,21 @@
+import fs from "fs";
+import path from "path";
+import zlib from "zlib";
+
 const decompress = async () => {
-    // Write your code here 
+  const inputFilePath = path.resolve("src", "zip", "files", "archive.gz");
+  const outputFilePath = path.resolve(
+    "src",
+    "zip",
+    "files",
+    "fileToCompress.txt"
+  );
+
+  const readStream = fs.createReadStream(inputFilePath);
+  const gunzipStream = zlib.createGunzip();
+  const writeStream = fs.createWriteStream(outputFilePath);
+
+  readStream.pipe(gunzipStream).pipe(writeStream);
 };
 
 await decompress();
